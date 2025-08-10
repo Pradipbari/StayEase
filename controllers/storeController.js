006a14c271c38a85a2720224ee88e2fdbabf289d
Pradip Bari <baripradip0@gmail.com>
2025-08-10 18:28:37 +0530

    Update storeController.js

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -6,7 +6,7 @@ exports.getIndex = (req, res, next) => {
   Home.find().then((registeredHomes) => {
     res.render("store/index", {
       registeredHomes: registeredHomes,
-      pageTitle: "Airbnb Home",
+      pageTitle: "StayEase",
       currentPage: "index",
       isLoggedIn: req.isLoggedIn,
       user: req.session.user,
